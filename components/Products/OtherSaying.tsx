d875728da74acc5a2fc6603c3495d8b139ebf218
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2024-12-30 13:14:06 +0430

    update

--- components/Products/OtherSaying.tsx
+++ components/Products/OtherSaying.tsx
@@ -1,33 +1,6 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-const otherSayingItems = [
-  {
-    source: "/images/products/other/1.mp4",
-    poster: "/images/products/other/1.jpg",
-  },
-  {
-    source: "/images/products/other/2.mp4",
-    poster: "/images/products/other/2.jpg",
-  },
-  {
-    source: "/images/products/other/3.mp4",
-    poster: "/images/products/other/3.jpg",
-  },
-  {
-    source: "/images/products/other/4.mp4",
-    poster: "/images/products/other/4.jpg",
-  },
-  {
-    source: "/images/products/other/5.mp4",
-    poster: "/images/products/other/5.jpg",
-  },
-  {
-    source: "/images/products/other/6.mp4",
-    poster: "/images/products/other/6.jpg",
-  },
-];
-
 const OtherSaying = ({ openPopup }: { openPopup: (value: string) => void }) => {
   return (
     <div className="w-full">
@@ -49,19 +22,84 @@ const OtherSaying = ({ openPopup }: { openPopup: (value: string) => void }) => {
           },
         }}
       >
-        {otherSayingItems.map((item, index) => (
-          <SwiperSlide key={`other-say-${Math.random()}`}>
-            <div
-              key={`video-${index}`}
-              className="w-[100px] overflow-hidden border-4 border-double border-primary-text rounded-lg"
-              onClick={() =>
-                openPopup(`/images/products/other/${index + 1}.mp4`)
-              }
-            >
-              <Image width="100" height="300" src={item.poster} alt="saying" />
-            </div>
-          </SwiperSlide>
-        ))}
+        <SwiperSlide>
+          <div
+            className="w-[100px] overflow-hidden border-4 border-double border-primary-text rounded-lg"
+            onClick={() => openPopup(`/images/products/other/1.mp4`)}
+          >
+            <Image
+              width="100"
+              height="300"
+              src="/images/products/other/1.jpg"
+              alt="saying"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-[100px] overflow-hidden border-4 border-double border-primary-text rounded-lg"
+            onClick={() => openPopup(`/images/products/other/2.mp4`)}
+          >
+            <Image
+              width="100"
+              height="300"
+              src="/images/products/other/2.jpg"
+              alt="saying"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-[100px] overflow-hidden border-4 border-double border-primary-text rounded-lg"
+            onClick={() => openPopup(`/images/products/other/3.mp4`)}
+          >
+            <Image
+              width="100"
+              height="300"
+              src="/images/products/other/3.jpg"
+              alt="saying"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-[100px] overflow-hidden border-4 border-double border-primary-text rounded-lg"
+            onClick={() => openPopup(`/images/products/other/4.mp4`)}
+          >
+            <Image
+              width="100"
+              height="300"
+              src="/images/products/other/4.jpg"
+              alt="saying"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-[100px] overflow-hidden border-4 border-double border-primary-text rounded-lg"
+            onClick={() => openPopup(`/images/products/other/5.mp4`)}
+          >
+            <Image
+              width="100"
+              height="300"
+              src="/images/products/other/5.jpg"
+              alt="saying"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="w-[100px] overflow-hidden border-4 border-double border-primary-text rounded-lg"
+            onClick={() => openPopup(`/images/products/other/6.mp4`)}
+          >
+            <Image
+              width="100"
+              height="300"
+              src="/images/products/other/6.jpg"
+              alt="saying"
+            />
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
